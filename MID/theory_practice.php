37c7e33b30fd8857f0527895cfd516da5f02e4a2
arindom <arindom793@gmail.com>
2025-11-09 21:23:23 +0600

    .

--- MID/theory_practice.php
+++ MID/theory_practice.php
@@ -5,13 +5,21 @@
     <style>
         .p{
             
-            background-color: black;
+            background-color: lightblue;
             width: 500px;
             height: 50vmax; /* 100vmax meaning full window coverage, 50vmax for half */
+            
             margin: 0 auto; /* auto center the content  */
         }
+        .p1{
+            
+            background-color: lightblue;
+            width: 590px;
+            height: 50px; /* 100vmax meaning full window coverage, 50vmax for half */
+            
+        }
         .body{
-            background-color: blueviolet;
+            background-color: DodgerBlue;
         }
 
 
@@ -19,7 +27,31 @@
 
 </head>
 <body class="body">
+<p>
+This paragraph
+contains a lot of lines
+in the source code,
+but the browser
+ignores it.
+</p>
+<hr>
+<p>
+This paragraph
+contains         a lot of spaces
+in the source         code,
+but the        browser
+ignores it.
+</p>
 
+<p>
+  My Bonnie lies over the ocean.
+
+  My Bonnie lies over the sea.
+
+  My Bonnie lies over the ocean.
+
+  Oh, bring back my Bonnie to me.
+</p>
 
 <h2>9/11/2025</h2>
 <p>I want to learn html and css if i can at least this is what i thinki can do as long as i dont procrastinate</p>
@@ -27,14 +59,64 @@
 
 <tagname>Content goes here...</tagname>
 
-<div class="p"></div>
+<div class="p" alt="Girl with a jacket"></div>
+<center>
+<div class="p1"></div>
+</center>
+
+<h1>HTML Text Formatting Examples</h1>
+
+    <p>
+        <b>&lt;b&gt;:</b> This tag just makes text <b>bold</b>. 
+        It doesn't imply extra importance.
+    </p>
+    <p>
+        <b>&lt;strong&gt;:</b> This tag makes text <strong>important</strong>, 
+        which browsers usually render as bold. Use this to indicate 
+        seriousness or urgency.
+    </p>
+
+    <p>
+        <b>&lt;i&gt;:</b> This tag makes text <i>italic</i>. 
+        It's often used for technical terms, foreign words, or thoughts.
+    </p>
+    <p>
+        <b>&lt;em&gt;:</b> This tag <em>emphasizes</em> text. 
+        Browsers usually render this as italic. Use it to stress a word.
+    </p>
+
+    <p>
+        <b>&lt;mark&gt;:</b> This tag <mark>marks or highlights</mark> text, 
+        like using a highlighter pen.
+    </p>
+
+    <p>
+        <b>&lt;small&gt;:</b> This tag makes text <small>smaller</small>, 
+        often used for side comments or copyright information.
+    </p>
+
+    <p>
+        <b>&lt;del&gt; and &lt;ins&gt;:</b> These are often used together 
+        to show edits. <br>
+        Example: The price is <del>$20.00</del> <ins>$15.00</ins>.
+    </p>
+
+    <p>
+        <b>&lt;sub&gt;:</b> This creates subscript text, which hangs lower. <br>
+        Example: The chemical formula for water is H<sub>2</sub>O.
+    </sP>
+
+    <p>
+        <b>&lt;sup&gt;:</b> This creates superscript text, which is higher. <br>
+        Example: The equation for the Pythagorean theorem is a<sup>2</sup> 
+        + b<sup>2</sup> = c<sup>2</sup>.
+    </p>
 
 
-
-
-
-
-
+<center>
+<h1 style="background-color:DodgerBlue;color: white;">Hello World</h1>
+<p style="background-color:Tomato; color: white;">Lorem ipsum...</p>
+</center>
 
 
 </body>
